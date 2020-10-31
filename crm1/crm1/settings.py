782d53559fc3a99b0d97b51f31e17c6cff264e8b
Menos  Velasco jr <menozvelasco1@gmail.com>
2020-10-30 17:09:25 -0700

    Password Reset Email

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -128,3 +128,13 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'manuelvelasco2020@gmail.com'
+EMAIL_HOST_PASSWORD = 'Spicebomb33@'
